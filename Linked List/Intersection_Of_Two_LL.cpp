5a0d02923a3d7e49ab3753b800b84d923d17ba67
rohit-pandey45 <rpandey1627@gmail.com>
2025-06-26 16:09:44 +0530

    Modified Linked List Folder

---
+++ Linked List/Intersection_Of_Two_LL.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL){
+            return NULL;
+        }
+        ListNode *temp1 = headA;
+        ListNode *temp2 = headB;
+        int count = 0;
+        while(temp1 != temp2){
+            if(count == 2) break;
+            if(temp1 == NULL) count ++;
+            temp1 = temp1 == NULL ? headB : temp1->next;
+            temp2 = temp2 == NULL ? headA : temp2->next;
+        }
+        if( temp1 != temp2) return NULL;
+        else
+            return temp1;
+    }
+};

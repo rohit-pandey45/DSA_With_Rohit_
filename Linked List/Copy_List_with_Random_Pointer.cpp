d6137bda2a8b5df4d75b328de8ce4f9e5f204dc2
rohit-pandey45 <rpandey1627@gmail.com>
2025-07-01 13:26:54 +0530

    Created Stack & Queue Folder

---
+++ Linked List/Copy_List_with_Random_Pointer.cpp
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    void insertCopyInBetween(Node* head){
+        Node* temp = head;
+        while(temp != NULL){
+            Node* nextele = temp->next;
+            Node* copy = new Node(temp->val);
+            copy->next = nextele;
+            temp->next = copy;
+            temp = nextele;
+        }
+    }
+    void connectRandomPointers(Node* head){
+        Node* temp = head;
+        while(temp != NULL){
+            Node* copyNode = temp->next;
+            if(temp->random){
+                copyNode->random = temp->random->next;
+            }
+            else{
+                copyNode->random = nullptr;
+            }
+            temp = temp->next->next;
+        }
+    }
+
+    Node* getDeepCopyList(Node* head){
+        Node* temp = head;
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+        while(temp != NULL){
+            res->next = temp->next;
+            res = res->next;
+            temp->next = temp->next->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+
+    Node* copyRandomList(Node* head) {
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
+    }
+};

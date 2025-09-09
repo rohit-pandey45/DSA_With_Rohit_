49489764bcb4a4b9e668cf25aceeace2fcd87cd8
rohit-pandey45 <rpandey1627@gmail.com>
2025-09-09 18:30:11 +0530

    POTD : Majority Element

--- Array/Majority_Element.cpp
+++ Array/Majority_Element.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //BRUTE FORCE APPROACH
+
+        // int n = nums.size();
+        // for(int i = 0; i<n; i++){
+        //     int ele = nums[i];
+        //     int cnt = 0;
+        //     for(int j = 0; j<n; j++){
+        //         if(nums[j] == ele){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt > n/2)   return ele;
+        // }
         int n = nums.size();
         int count  = 0;
         int ele = 0;

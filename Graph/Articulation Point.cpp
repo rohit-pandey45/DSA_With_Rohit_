3ebe4a1eeaaed263ef3b7151aebbfcd7cc665926
rohit-pandey45 <rpandey1627@gmail.com>
2025-11-01 01:38:42 +0530

    POTD : Articulation Point

---
+++ Graph/Articulation Point.cpp
@@ -0,0 +1,58 @@
+// User function Template for C++
+
+class Solution {
+private:
+    int timer = 0;
+
+    void dfs(int node, int parent, vector<int> &vis, vector<int> adj[],
+             int tin[], int low[], vector<int> &mark) {
+        
+        vis[node] = 1;
+        tin[node] = low[node] = timer++;
+        int child = 0;
+
+        for (auto it : adj[node]) {
+            if (it == parent) continue;
+
+            if (!vis[it]) {
+                dfs(it, node, vis, adj, tin, low, mark);
+                low[node] = min(low[node], low[it]);
+
+                if (low[it] >= tin[node] && parent != -1)
+                    mark[node] = 1;
+
+                child++; 
+            }
+            else {
+                low[node] = min(low[node], tin[it]);
+            }
+        }
+
+        
+        if (parent == -1 && child > 1)
+            mark[node] = 1;
+    }
+
+public:
+    vector<int> articulationPoints(int n, vector<int> adj[]) {
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<int> mark(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(i, -1, vis, adj, tin, low, mark);
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (mark[i] == 1)
+                ans.push_back(i);
+        }
+
+        if (ans.empty()) return {-1};
+        return ans;
+    }
+};

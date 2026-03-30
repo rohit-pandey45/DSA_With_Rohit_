4ff5046d3af12cf704dc24ab2905d297474abc90
Rohit Pandey <rpandey1627@gmail.com>
2026-03-30 20:31:10 +0000

    Revision

--- Binary Search/Search_2D_matrix.cpp
+++ Binary Search/Search_2D_matrix.cpp
@@ -23,3 +23,41 @@ public:
         return false;
     }
 };
+// class Solution {
+// private:
+//     bool binarySearch(vector<int>& matrix, int target){
+//         int low = 0;
+//         int high = matrix.size()-1;
+//         while(low <= high){
+//             int mid = low + ((high - low)/2);
+//             if(matrix[mid] == target){
+//                 return true;
+//             }
+//             else if(matrix[mid] < target){
+//                 low = mid + 1;
+//             }
+//             else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return false;
+//     }
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         int low = 0;
+//         int high = n-1;
+//         // int high = (m*n) - 1;
+//         while(low <= high){
+//             int mid = low + ((high - low)/2);
+//             for(int i = 0; i<m; i++){
+//                 if(matrix[i][0] <= target && target <= matrix[i][n-1]){
+//                     return binarySearch(matrix[i],target);
+//                 }
+//             }
+
+//         }
+//         return false;
+//     }
+// };
